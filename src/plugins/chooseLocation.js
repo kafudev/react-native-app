734374390fda8066db68e9ca06fb14b150804f12
jude <zpd106@sina.com>
2022-10-22 21:25:31 +0800

    up

--- src/plugins/chooseLocation.js
+++ src/plugins/chooseLocation.js
@@ -24,6 +24,8 @@ import Modal from './Modal';
 const {width, height} = Dimensions.get('screen');
 const App = ({navigation}) => {
   const refMap = useRef(null);
+  const [lng, setLng] = useState(116.37296);
+  const [lat, setLat] = useState(39.91095);
   useEffect(() => {
     AMapSdk.init(
       Platform.select({
@@ -50,22 +52,37 @@ const App = ({navigation}) => {
       <MapView
         ref={refMap}
         mapType={MapType.Standard}
+        myLocationEnabled={true}
         compassEnabled={false}
         zoomControlsEnabled={true}
         myLocationButtonEnabled={true}
         rotateGesturesEnabled={false}
         tiltGesturesEnabled={false}
-        initialCameraPosition={{
-          // target: {
-          //   latitude: 39.91095,
-          //   longitude: 116.37296,
-          // },
-          zoom: 14,
+        cameraPosition={{
+          latitude: lat,
+          longitude: lng,
         }}
-        onLoad={() => console.log('onLoad')}
-        onPress={e => console.log('onPress', e)}
-        // onCameraIdle={e => console.log('onCameraIdle', e)}
-        onLocation={e => console.log('onLocation', e)}
+        // initialCameraPosition={{
+        //   // target: {
+        //   //   latitude: lat,
+        //   //   longitude: lng,
+        //   // },
+        //   zoom: 16,
+        // }}
+        onLoad={() => {
+          console.log('onLoad');
+        }}
+        onPress={e => {
+          console.log('onPress', e.nativeEvent);
+        }}
+        onCameraIdle={e => {
+          console.log('onCameraIdle', e.nativeEvent);
+        }}
+        onLocation={e => {
+          console.log('onLocation', e.nativeEvent);
+          e.nativeEvent && setLng(e.nativeEvent.coords.longitude);
+          e.nativeEvent && setLat(e.nativeEvent.coords.latitude);
+        }}
       />
       <Button title="关闭地图选择" onPress={() => Modal.close()} />
     </View>
